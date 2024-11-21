607fd9196403cc06cabd0133e23b075a74d3bb30
Ivan Kosach <kosaci74@gmail.com>
2024-11-21 18:58:44 +0300

    fix

--- core/assistant/core/config.py
+++ core/assistant/core/config.py
@@ -1,3 +1,5 @@
+import logging
+import os
 
 import dotenv
 from aiogram import Dispatcher
@@ -6,3 +8,11 @@ from aiogram.fsm.storage.redis import RedisStorage
 dotenv.load_dotenv()
 storage = RedisStorage.from_url('redis://localhost:6379/5')
 dp = Dispatcher(storage=storage)
+
+if os.environ.get('DEBUG', '').lower() == 'false':
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    logging.basicConfig(level=logging.WARNING, filename="core/static/logs.log", filemode="a",
+                        format=f"ASSISTANT {os.environ.get('ASSISTANT_ID')} %(asctime)s %(levelname)s "
+                               f"%(message)s\n" + '_' * 100)
