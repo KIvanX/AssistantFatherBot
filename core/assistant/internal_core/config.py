559aaed386bca2c16ff1e5dff3915c734556d652
Ivan Kosach <kosaci74@gmail.com>
2025-02-01 16:47:51 +0300

    fix

--- core/assistant/internal_core/config.py
+++ core/assistant/internal_core/config.py
@@ -23,7 +23,6 @@ if not DEBUG:
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
 
-    print('init')
     logging.basicConfig(level=logging.WARNING, filename=f"core/static/logs.log", filemode="a",
                         format=f"ASSISTANT {os.environ.get('ASSISTANT_ID')} %(asctime)s %(levelname)s "
                                f"%(message)s\n" + '_' * 100)
